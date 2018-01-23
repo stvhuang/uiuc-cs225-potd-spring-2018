d2e426a65d36a7e5e5fb985693561e3cc8f5883a
Yanjia Huang <yanjia.huang.tw@gmail.com>
2018-01-23 10:04:17 -0600

    Finish potd-q2

--- potd-q2/epoch.cpp
+++ potd-q2/epoch.cpp
@@ -1,3 +1,14 @@
 /* Your code here! */
 #include "epoch.h"
 
+int hours(time_t t) {
+    return t / (60 * 60);
+}
+
+int days(time_t t) {
+    return hours(t) / 24;
+}
+
+int years(time_t t) {
+    return days(t) / 365;
+}
