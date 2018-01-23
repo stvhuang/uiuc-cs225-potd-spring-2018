b7988ba4e262188e8f28cbe700e8ee99ac9102ac
Yanjia Huang <yanjia.huang.tw@gmail.com>
2018-01-23 09:27:40 -0600

    Finish potd-q1

--- potd-q1/hello.cpp
+++ potd-q1/hello.cpp
@@ -1,1 +1,10 @@
 /* Your code here! */
+
+#include "hello.h"
+
+std::string hello() {
+    std::string myName = "Yan-Jia";
+    unsigned int myAge = 22;
+
+    return "Hello world! My name is " + myName + " and I am " + std::to_string(myAge) + " years old.";
+}
