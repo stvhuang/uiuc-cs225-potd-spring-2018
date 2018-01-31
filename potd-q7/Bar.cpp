3b4907a701c3ae12681565f78abf3b1c9928e444
Yan-Jia Huang <yanjia.huang.tw@gmail.com>
2018-01-31 14:37:21 -0600

    Small fix potd-q7

--- potd-q7/Bar.cpp
+++ potd-q7/Bar.cpp
@@ -2,26 +2,26 @@
 
 #include "Bar.h"
 
-using namespace potd;
+namespace potd {
+    Bar::Bar(string name) {
+        f_ = new Foo(name);
+    }
 
-Bar::Bar(string name) {
-    f_ = new Foo(name);
-}
+    Bar::Bar(const Bar &other) {
+        f_ = new Foo(other.get_name());
+    }
 
-Bar::Bar(const Bar &other) {
-    f_ = new Foo(other.get_name());
-}
+    Bar::~Bar() {
+        delete f_;
+    }
 
-Bar::~Bar() {
-    delete f_;
-}
+    Bar & Bar::operator=(const Bar &other) {
+        delete f_;
+        f_ = new potd::Foo(other.get_name());
+        return *this;
+    }
 
-Bar & Bar::operator=(const Bar &other) {
-    delete f_;
-    f_ = new potd::Foo(other.get_name());
-    return *this;
-}
-
-string Bar::get_name() const {
-    return f_->get_name();
+    string Bar::get_name() const {
+        return f_->get_name();
+    }
 }
