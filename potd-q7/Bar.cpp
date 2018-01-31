2a2c3b66f5aa8bd7f79fd5bca6502ad8c67f5fc9
Yan-Jia Huang <yanjia.huang.tw@gmail.com>
2018-01-30 19:26:31 -0600

    Finish potd-q7

--- potd-q7/Bar.cpp
+++ potd-q7/Bar.cpp
@@ -1,1 +1,27 @@
 // your code here
+
+#include "Bar.h"
+
+using namespace potd;
+
+Bar::Bar(string name) {
+    f_ = new Foo(name);
+}
+
+Bar::Bar(const Bar &other) {
+    f_ = new Foo(other.get_name());
+}
+
+Bar::~Bar() {
+    delete f_;
+}
+
+Bar & Bar::operator=(const Bar &other) {
+    delete f_;
+    f_ = new potd::Foo(other.get_name());
+    return *this;
+}
+
+string Bar::get_name() const {
+    return f_->get_name();
+}
--- potd-q7/Bar.h
+++ potd-q7/Bar.h
@@ -1,1 +1,24 @@
 // your code here
+
+#ifndef BAR_H_
+#define BAR_H_
+
+#include <string>
+#include "Foo.h"
+
+using namespace std;
+
+namespace potd {
+    class Bar {
+        public:
+            Bar(string);
+            Bar(const Bar &);
+            ~Bar();
+            Bar & operator=(const Bar &);
+            string get_name() const;
+        private:
+            Foo * f_;
+    };
+}
+
+#endif
