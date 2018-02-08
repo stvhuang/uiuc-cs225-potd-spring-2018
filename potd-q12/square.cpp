7187218163b333b44c7f20be7122a426d3f9f784
Yan-Jia Huang <yanjia.huang.tw@gmail.com>
2018-02-08 00:41:28 -0600

    Add potd-q12

---
+++ potd-q12/square.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "square.h"
+
+Square::Square() {
+    name = "mysquare";
+    lengthptr = new double;
+    *lengthptr = 2.0;
+}
+
+void Square::setName(string newName) {
+  name = newName;
+}
+
+void Square::setLength(double newVal) {
+  *lengthptr = newVal;
+}
+
+string Square::getName() const {
+  return name;
+}
+
+double Square::getLength() const {
+  return *lengthptr;
+}
+
+Square::Square(const Square & other) {
+    name = other.getName();
+    lengthptr = new double;
+    *lengthptr = other.getLength();
+}
+
+Square::~Square() {
+    delete lengthptr;
+}
+
