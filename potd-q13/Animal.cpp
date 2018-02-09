c9ecf789547072aca3d8b3d83d81924e7311cf4e
Yan-Jia Huang <yanjia.huang.tw@gmail.com>
2018-02-09 00:25:16 -0600

    Finish potd-q13

--- potd-q13/Animal.cpp
+++ potd-q13/Animal.cpp
@@ -1,1 +1,18 @@
 // Animal.cpp
+
+#include "Animal.h"
+
+Animal::Animal() : type_("cat"), food_("fish") {};
+
+Animal::Animal(std::string type, std::string food)
+  : type_(type), food_(food) {}
+
+std::string Animal::getType() { return type_; }
+
+void Animal::setType(std::string type) { type_ = type; }
+
+std::string Animal::getFood() { return food_; }
+
+void Animal::setFood(std::string food) { food_ = food; }
+
+std::string Animal::print() { return "I am a " + type_; }
--- potd-q13/Animal.h
+++ potd-q13/Animal.h
@@ -1,1 +1,22 @@
 // Animal.h
+
+#ifndef ANIMAL_H_
+#define ANIMAL_H_
+
+#include <iostream>
+
+class Animal {
+ public:
+  Animal();
+  Animal(std::string type, std::string food);
+  std::string getType();
+  void setType(std::string type);
+  std::string getFood();
+  void setFood(std::string food);
+  virtual std::string print();
+ protected:
+  std::string type_;
+  std::string food_;
+};
+
+#endif
