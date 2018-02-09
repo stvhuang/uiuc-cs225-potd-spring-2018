4b819a8fdfd8cb8ff3d4cb8095685c90624ff829
Yan-Jia Huang <yanjia.huang.tw@gmail.com>
2018-02-09 13:53:40 -0600

    Finish potd-q14

--- potd-q14/Pet.cpp
+++ potd-q14/Pet.cpp
@@ -1,1 +1,39 @@
 // Pet.cpp
+
+#include "Pet.h"
+
+Pet::Pet() : Animal("cat", "fish"), name_("Fluffy"), owner_name_("Cinda") {}
+
+Pet::Pet(string type, string food, string name, string owner_name)
+  : Animal(type, food), name_(name), owner_name_(owner_name) {}
+
+void Pet::setFood(string food) {
+  food_ = food;
+  return;
+}
+
+string Pet::getFood() {
+  return food_;
+}
+
+void Pet::setName(string name) {
+  name_ = name;
+  return;
+}
+
+string Pet::getName() {
+  return name_;
+}
+
+void Pet::setOwnerName(string owner_name) {
+  owner_name_ = owner_name;
+  return;
+}
+
+string Pet::getOwnerName() {
+  return owner_name_;
+}
+
+string Pet::print() {
+  return "My name is " + name_;
+}
--- potd-q14/Pet.h
+++ potd-q14/Pet.h
@@ -1,1 +1,24 @@
 // Pet.h
+
+#ifndef PET_H_
+#define PET_H_
+
+#include "Animal.h"
+
+class Pet : public Animal {
+ public:
+  Pet();
+  Pet(string type, string food, string name, string owner_name);
+  void setFood(string food);
+  string getFood();
+  void setName(string name);
+  string getName();
+  void setOwnerName(string owner_name);
+  string getOwnerName();
+  string print();
+ private:
+  string name_;
+  string owner_name_;
+};
+
+#endif
