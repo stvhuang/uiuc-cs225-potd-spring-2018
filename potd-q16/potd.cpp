1ddd4138ee3e6f9190d8540c590916d2d50315dc
Yan-Jia Huang <yanjia.huang.tw@gmail.com>
2018-02-13 11:05:41 -0600

    Finish potd-q16

--- potd-q16/potd.cpp
+++ potd-q16/potd.cpp
@@ -5,4 +5,24 @@ using namespace std;
 
 string stringList(Node *head) {
     // your code here!
+    unsigned count = 0;
+    string result = "";
+
+    if (head == NULL) {
+        return "Empty list";
+    } else {
+        while (1) {
+            result += "Node ";
+            result += to_string(count++);
+            result += ": ";
+            result += to_string(head->data_);
+            if (head->next_ == NULL) {
+                return result;
+            } else {
+                result += " -> ";
+                head = head->next_;
+                continue;
+            }
+        }
+    }
 }
