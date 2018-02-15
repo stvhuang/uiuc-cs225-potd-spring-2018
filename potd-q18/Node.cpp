0494d8e10aa0e40ed8aa2f454a45c311fd46d55e
Yan-Jia Huang <yanjia.huang.tw@gmail.com>
2018-02-15 13:25:39 -0600

    Add potd-q18

---
+++ potd-q18/Node.cpp
@@ -0,0 +1,24 @@
+#include "Node.h"
+
+using namespace std;
+
+void sortList(Node **head) {
+  // your code here!
+}
+
+Node::Node() {
+    numNodes++;
+}
+
+Node::Node(Node &other) {
+    this->data_ = other.data_;
+    this->next_ = other.next_;
+    numNodes++;
+}
+
+Node::~Node() {
+    numNodes--;
+}
+
+int Node::numNodes = 0;
+
