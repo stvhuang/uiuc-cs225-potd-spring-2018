b4db153077193e8c7b85fdc145d2a25be02b633a
Yan-Jia Huang <yanjia.huang.tw@gmail.com>
2018-02-16 20:00:26 -0600

    Finish potd-q19

--- potd-q19/Node.cpp
+++ potd-q19/Node.cpp
@@ -1,9 +1,33 @@
 #include "Node.h"
-
+#include <iostream>
 using namespace std;
 
 void mergeList(Node *first, Node *second) {
   // your code here!
+  Node *f = first;
+  Node *tmp = NULL;
+
+  if (first == NULL && second == NULL) {
+    return;
+  } else if (first == NULL && second != NULL) {
+    first = second;
+    return;
+  }
+
+  while (1) {
+    if (f->next_ == NULL) {
+      f->next_ = second;
+      return;
+    } else if (second == NULL) {
+      return;
+    } else {
+      tmp = second->next_;
+      second->next_ = f->next_;
+      f->next_ = second;
+      second = tmp;
+      f = f->next_->next_;
+    }
+  }
 }
 
 Node::Node() {
