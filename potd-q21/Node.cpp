907bb008a405c61b1d3ce5db2abdf0dff29c1d3b
Yan-Jia Huang <yanjia.huang.tw@gmail.com>
2018-02-20 10:17:55 -0600

    Finish potd-q21

--- potd-q21/Node.cpp
+++ potd-q21/Node.cpp
@@ -2,9 +2,42 @@
 
 using namespace std;
 
+bool isInList(Node *head, int data) {
+  while (head != NULL) {
+    if (head->data_ == data) {
+      return true;
+    }
+    head = head->next_;
+  }
+
+  return false;
+}
+
 Node *listIntersection(Node *first, Node *second) {
   // your code here
-  return NULL;
+  Node *head = NULL;
+  Node *curr = NULL;
+
+  if ((first == NULL) || (second == NULL)) {
+    return NULL;
+  } else {  // (first != NULL) && (second != NULL)
+    while (first != NULL) {
+      if (isInList(second, first->data_) && !(isInList(head, first->data_))) {
+        if (head == NULL) {
+          head = new Node(*first);
+          head->next_ = NULL;
+          curr = head;
+        } else {
+          curr->next_ = new Node(*first);
+          curr = curr->next_;
+          curr->next_ = NULL;
+        }
+      }
+      first = first->next_;
+    }
+  }
+
+  return head;
 }
 
 Node::Node() {
