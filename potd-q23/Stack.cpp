a2b5f0278407e22907c379e0f90c05f4e9848e70
Yan-Jia Huang <yanjia.huang.tw@gmail.com>
2018-02-22 23:50:07 -0600

    Finish potd-q23

--- potd-q23/Stack.cpp
+++ potd-q23/Stack.cpp
@@ -1,21 +1,42 @@
 #include "Stack.h"
 
+Stack::Stack() : top_(NULL) {}
+
 // `int size()` - returns the number of elements in the stack (0 if empty)
 int Stack::size() const {
-  return 0;
+  int count = 0;
+  Node *temp = top_;
+  while (temp != NULL) {
+    ++count;
+    temp = temp->prev_;
+  }
+  return count;
 }
 
 // `bool isEmpty()` - returns if the list has no elements, else false
 bool Stack::isEmpty() const {
-  return true;
+  if (top_ == NULL) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // `void push(int val)` - pushes an item to the stack in O(1) time
 void Stack::push(int value) {
-
+  Node *temp  = new Node(value);
+  temp->prev_ = top_;
+  top_ = temp;
+  temp = NULL;
+  return;
 }
 
 // `int pop()` - removes an item off the stack and returns the value in O(1) time
 int Stack::pop() {
-  return -1;
+  Node *temp = top_;
+  int result = temp->data_;
+  top_ = top_->prev_;
+  delete temp;
+  temp = NULL;
+  return result;
 }
--- potd-q23/Stack.h
+++ potd-q23/Stack.h
@@ -5,10 +5,20 @@
 
 class Stack {
 public:
+  Stack();
   int size() const;
   bool isEmpty() const;
   void push(int value);
   int pop();
+private:
+  class Node {
+    public:
+      Node(int data) : data_(data), prev_(NULL) {}
+      int data_;
+      Node *prev_;
+  };
+
+  Node *top_;
 };
 
 #endif
