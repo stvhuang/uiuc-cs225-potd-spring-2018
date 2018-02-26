45756e3baffee1371d6fe09bc6f3d9baa5813b3e
Yan-Jia Huang <yanjia.huang.tw@gmail.com>
2018-02-25 18:23:07 -0600

    Finish potd-q24

--- potd-q24/isValid.cpp
+++ potd-q24/isValid.cpp
@@ -5,5 +5,35 @@ using namespace std;
 
 bool isValid(string input) {
     stack<char> st;
-    return true;    // modify this!
+    int l = input.length();
+
+    for (int i = 0; i < l; ++i) {
+        if (input[i] == '(' || input[i] == '[' || input[i] == '{') {
+            st.push(input[i]);
+        } else if (input[i] == ')') {
+            if (!st.empty() && st.top() == '(') {
+                st.pop();
+            } else {
+                return false;
+            }
+        } else if (input[i] == ']') {
+            if (!st.empty() && st.top() == '[') {
+                st.pop();
+            } else {
+                return false;
+            }
+        } else if (input[i] == '}') {
+            if (!st.empty() && st.top() == '{') {
+                st.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    if (st.empty()) {
+        return true;
+    } else {
+        return false;
+    }
 }
