de71de7e2349025af7f826455808aed4ff57f13d
Yan-Jia Huang <yanjia.huang.tw@gmail.com>
2018-02-26 15:17:06 -0600

    Finish potd-q25

--- potd-q25/Queue.h
+++ potd-q25/Queue.h
@@ -5,10 +5,23 @@
 
 class Queue {
     public:
+        Queue();
         int size() const;
         bool isEmpty() const;
         void enqueue(int value);
         int dequeue();
+
+    private:
+        class Node {
+            public:
+                Node() : data(0), next(NULL) {};
+                Node (int value) : data(value), next(NULL) {};
+                int data;
+                Node *next;
+        };
+
+        Node *front;
+        Node *rear;
 };
 
 #endif
