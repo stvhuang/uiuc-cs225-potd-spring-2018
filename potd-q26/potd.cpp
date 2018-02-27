f7fd3111b057d73dfb1121b7f8cec09d382a0966
Yan-Jia Huang <yanjia.huang.tw@gmail.com>
2018-02-27 13:40:32 -0600

    Finish potd-q26

--- potd-q26/potd.cpp
+++ potd-q26/potd.cpp
@@ -1,1 +1,20 @@
 // Your code here
+
+#include "potd.h"
+
+string getFortune(const string &s) {
+    unsigned res = s.size() % 5;
+
+    switch (res) {
+        case 1:
+            return "As you wish!";
+        case 2:
+            return "Nec spe nec metu!";
+        case 3:
+            return "Do, or do not. There is no try.";
+        case 4:
+            return "This fortune intentionally left blank.";
+        default:
+            return "Amor Fati!";
+    }
+}
