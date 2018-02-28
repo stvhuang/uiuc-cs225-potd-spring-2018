550b64f04e8b4127a53508911603c3c8793fff9a
Yan-Jia Huang <yanjia.huang.tw@gmail.com>
2018-02-28 16:06:23 -0600

    Finish potd-q27

--- potd-q27/TreeNode.cpp
+++ potd-q27/TreeNode.cpp
@@ -2,10 +2,19 @@
 
 #include <cstddef>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 TreeNode::TreeNode() : left_(NULL), right_(NULL) { }
 
 int TreeNode::getHeight() {
-  return -1;
+  if (left_ == NULL && right_ == NULL) {
+    return 0;
+  } else if (left_ != NULL && right_ != NULL) {
+    return 1 + max(left_->getHeight(), right_->getHeight());
+  } else if (left_ != NULL && right_ == NULL) {
+    return 1 + left_->getHeight();
+  } else {  // left == NULL && right != NULL
+    return 1 + right_->getHeight();
+  }
 }
