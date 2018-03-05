2bc33ce1cbd404db9fc5e5d739241d9cf41a0c04
Yan-Jia Huang <yanjia.huang.tw@gmail.com>
2018-03-04 21:56:48 -0600

    Finish potd-q28
    Got 80/100 points.

--- potd-q28/TreeNode.cpp
+++ potd-q28/TreeNode.cpp
@@ -1,11 +1,108 @@
 #include "TreeNode.h"
 #include <iostream>
+using namespace std;
 
-TreeNode * deleteNode(TreeNode* root, int key) {
-  // your code here
-  return root;
+
+TreeNode * findIOP(TreeNode *subRoot) {
+  TreeNode *curr = subRoot->left_;
+  TreeNode *part = NULL;
+
+  if (subRoot->right_ == NULL) {
+    // subRoot is the IOP
+    return NULL;
+  }
+
+  while (curr->right_ != NULL) {
+    part = curr;
+    curr = curr->right_;
+  }
+
+  return part;
 }
 
+TreeNode * deleteNode(TreeNode* root, int key) {
+
+  // your code here
+  TreeNode * part = NULL;
+  TreeNode * track = root;
+
+  if (root->val_ == key) {
+    return root;
+  }
+
+  while (track->val_ != key) {
+    part = track;
+    if (track->val_ > key) {
+      track = track->left_;
+    } else {
+      track = track->right_;
+    }
+  }
+
+  if (track->val_ != key) {
+    cout << "SOMETHING WRONG!!!" << endl;
+  }
+
+  if (track->left_ == NULL && track->right_ == NULL) {
+    // track is a leaf
+    if (part->left_ == track) {
+      part->left_ = NULL;
+      delete track;
+      track = NULL;
+      return root;
+    } else {
+      part->right_ = NULL;
+      delete track;
+      track = NULL;
+      return root;
+    }
+  } else if (track->left_ != NULL && track->right_ == NULL) {
+    // track has only one child, left child
+    if (part->left_ == track) {
+      part->left_ = track->left_;
+      delete track;
+      track = NULL;
+      return root;
+    } else {
+      part->right_ = track->left_;
+      delete track;
+      track = NULL;
+      return root;
+    }
+  } else if (track->left_ == NULL && track->right_ != NULL) {
+    // track has only one child, left child
+    if (part->left_ == track) {
+      part->left_ = track->right_;
+      delete track;
+      track = NULL;
+      return root;
+    } else {
+      part->right_ = track->right_;
+      delete track;
+      track = NULL;
+      return root;
+    }
+  } else {
+    // track has two children, use IOP
+    TreeNode *IOP = findIOP(track);
+    if (IOP == NULL) {
+      int temp = track->val_;
+      track->val_ = track->left_->val_;
+      track->left_->val_ = temp;
+      if (track->left_->left_ == NULL && track->left_->right_ == NULL) {
+        // is a leaf node
+        delete track->left_;
+        track->left_ = NULL;
+      } else {
+        deleteNode(track->left_, key);
+      }
+      return root;
+    }
+
+    deleteNode(track, key);
+    return root;
+  }
+}
 void inorderPrint(TreeNode* node)
 {
     if (!node)  return;
