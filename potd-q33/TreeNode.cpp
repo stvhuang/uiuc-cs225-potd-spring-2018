c2034b6f70ccf8e30ba4fc59a390464fccbb4162
Yan-Jia Huang <yanjia.huang.tw@gmail.com>
2018-03-08 21:40:18 -0600

    Finish potd-q33

--- potd-q33/TreeNode.cpp
+++ potd-q33/TreeNode.cpp
@@ -1,21 +1,33 @@
 #include "TreeNode.h"
 #include <algorithm>
 
-
 void rightRotate(TreeNode* root) {
 
     // Your code here
+    TreeNode *temp = root->left_;
+    root->left_ = root->left_->right_;
+    root->left_->parent_ = root;
+    temp->parent_ = NULL;
+    temp->right_ = root;
+    root->parent_ = temp;
+    temp = NULL;
 
+    return;
 }
-
 
 void leftRotate(TreeNode* root) {
 
     // your code here
+    TreeNode *temp = root->right_;
+    root->right_ = root->right_->left_;
+    root->right_->parent_ = root;
+    temp->parent_ = NULL;
+    temp->left_ = root;
+    root->parent_ = temp;
+    temp = NULL;
 
+    return;
 }
-
-
 
 void deleteTree(TreeNode* root)
 {
@@ -25,4 +37,3 @@ void deleteTree(TreeNode* root)
   delete root;
   root = NULL;
 }
-
