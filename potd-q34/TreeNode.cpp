bbf05cf26c6e3e93a0319aadeb5ef1e0b9c12664
Yan-Jia Huang <yanjia.huang.tw@gmail.com>
2018-03-12 04:03:08 -0500

    Finish potd-q34

--- potd-q34/TreeNode.cpp
+++ potd-q34/TreeNode.cpp
@@ -2,5 +2,20 @@
 
 TreeNode::RotationType balanceTree(TreeNode*& subroot) {
 	// Your code here
-  return TreeNode::right;
+  int balance = getHeight(subroot->left_) - getHeight(subroot->right_);
+  if (balance == 2) {
+    int lbalance = getHeight(subroot->left_->left_) - getHeight(subroot->left_->right_);
+    if (lbalance == 1) {
+      return TreeNode::right;
+    } else if (lbalance == -1) {
+      return TreeNode::leftRight;
+    }
+  } else if (balance == -2) {
+    int lbalance = getHeight(subroot->right_->left_) - getHeight(subroot->right_->right_);
+    if (lbalance == 1) {
+      return TreeNode::rightLeft;
+    } else if (lbalance == -1) {
+      return TreeNode::left;
+    }
+  }
 }
