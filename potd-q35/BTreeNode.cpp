ec1a4398278435b1bfaaee37a9d4c7f734400cc5
Yan-Jia Huang <yanjia.huang.tw@gmail.com>
2018-03-12 16:24:13 -0500

    Finish potd-q35

--- potd-q35/BTreeNode.cpp
+++ potd-q35/BTreeNode.cpp
@@ -1,8 +1,25 @@
 #include <vector>
 #include "BTreeNode.h"
 
-
 BTreeNode* find(BTreeNode* root, int key) {
     // Your Code Here
-  return NULL;
+    if (root->children_.size() == 0) {
+        // Leaf node, no children
+        for (unsigned i = 0; i < root->elements_.size(); ++i) {
+            if (root->elements_[i] == key) {
+                return root;
+            }
+        }
+        return NULL;
+    } else {
+        // Non-leaf note
+        for (unsigned i = 0; i < root->elements_.size(); ++i) {
+            if (root->elements_[i] > key) {
+                return find(root->children_[i], key);
+            } else if (root->elements_[i] == key) {
+                return root;
+            }
+        }
+        return find(root->children_[root->children_.size() - 1], key);
+    }
 }
