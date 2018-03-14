f6e51cc1446765b3c0a5e334881bdeb163c07d8e
Yan-Jia Huang <yanjia.huang.tw@gmail.com>
2018-03-13 20:17:44 -0500

    Finish potd-q36

--- potd-q36/BTreeNode.cpp
+++ potd-q36/BTreeNode.cpp
@@ -1,9 +1,24 @@
 #include <vector>
 #include "BTreeNode.h"
 
+void traverse(BTreeNode *subRoot, std::vector<int> &v) {
+    if (subRoot->children_.size() == 0) {  // Leaf node
+        for (unsigned i = 0; i < subRoot->elements_.size(); ++i) {
+            v.push_back(subRoot->elements_[i]);
+        }
+    } else {  // Non-leaf node
+        for (unsigned i = 0; i < subRoot->elements_.size(); ++i) {
+            traverse(subRoot->children_[i], v);
+            v.push_back(subRoot->elements_[i]);
+        }
+        traverse(subRoot->children_[subRoot->children_.size() - 1], v);
+    }
+    return;
+}
 
 std::vector<int> traverse(BTreeNode* root) {
     // your code here
     std::vector<int> v;
+    traverse(root, v);
     return v;
 }
